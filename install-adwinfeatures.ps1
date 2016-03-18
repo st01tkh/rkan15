5074bef040164b5ae575a214619cb9e0aecec6e9
st01tkh <st01tkh@gmail.com>
2016-03-18 05:16:56 +0300

    Added Active Domain, DNS, DHCP Windows Features, Remote Desktop Enable

---
+++ install-adwinfeatures.ps1
@@ -0,0 +1,16 @@
+#################################################################################
+# Install IIS, MSMQ, WCF, .NET 4.5
+#################################################################################
+
+# To verify the installation this could use Get-WindowsFeature
+Write-Host "Installing Web server role features (IIS, MSMQ, .NET 4.5)"
+Install-WindowsFeature AD-Domain-Services
+Install-WindowsFeature DNS
+Install-WindowsFeature DHCP
+
+#################################################################################
+# Enable remote desktop and some default Windows explorer features
+#################################################################################
+Write-Host "Enabling remote desktop"
+netsh advfirewall firewall add rule name="Open Port 3389" dir=in action=allow protocol=TCP localport=3389
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
